8515dc1b907c538361464af266bb794e5dca64ac
Samuel Lemos <samuelt.lemos@hotmail.com>
2023-11-17 18:14:46 -0300

    feat: estilizando a página de login e implementando redirecionamento para a página de cadastro

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -42,7 +42,7 @@ export default function Login() {
     }
     else{
       await login(data)
-      console.log("Autenticado:", autenticado())
+      //console.log("Autenticado:", autenticado())
       autenticado() ? navigate("/home") : navigate("/")
       
       //autenticado === true ? navigate("/demandas") : null
@@ -50,31 +50,32 @@ export default function Login() {
   }
 
   return (
-    <div className="fixed inset-0 flex bg-white justify-center items-center">
+    <div className="fixed inset-0 flex bg-blue-100 justify-center items-center">
       {loader()}
-      <div className="flex">
-        <form onSubmit={(e) => {handleSubmit(e)}} className="flex flex-col space-y-4 items-center">
-          <div className="flex flex-col w-full px-2">
-            <label className="text-xs">Email</label>
-            <input type="email" name="email" id="email" onChange={(e) => setEmail(e.target.value)} className="border py-1 rounded" />
+      <div className="shadow-lg z-40 w-1/3 h-1/2 p-11 rounded-2xl bg-blue-200">
+        <form onSubmit={(e) => {handleSubmit(e)}} className="grid grid-rows-4 space-y-6">
+          <div className="flex space-x-3 items-center">
+            <label className="text-white font-semibold">Email</label>
+            <input type="email" name="email" id="email" onChange={(e) => setEmail(e.target.value)} className="w-full py-1 rounded hover:bg-gray-200 duration-150" />
           </div>
 
-          <div className="flex flex-col w-full px-2">
-            <label className="text-xs">Senha</label>
-            <div className="flex justify-between space-x-1.5 items-center">
-              <input type={showPassword ? "text" : "password"} name="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} className="border py-1 w-full rounded" />
-              <img src={showPassword ? ocultar : ver} className="w-5 h-5 hover:scale-110 cursor-pointer" onClick={handlePasswordToggle} />
-            </div>
+          <div className="flex space-x-3 items-center">
+          <label className="text-white font-semibold">Senha</label>
+            <input type={showPassword ? "text" : "password"} name="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} className="py-1 w-full rounded hover:bg-gray-200 duration-150" />
+            <img src={showPassword ? ocultar : ver} className="w-5 h-5 cursor-pointer hover:scale-110 duration-150" onClick={handlePasswordToggle} />
+          </div>
+          <div className='flex space-x-5 px-2'>
+          <button type="submit" className=" bg-green-300 w-1/2 text-white text-center font-semibold rounded-lg shadow-lg hover:scale-105 duration-75">
+            Login
+          </button>
+          <button type="button" onClick={() => navigate('/cadastro')} className=" bg-pink-300 w-1/2 text-white text-center font-semibold rounded-lg shadow-lg hover:scale-105 duration-75">
+            Cadastre-se
+          </button>
           </div>
 
-          <div className="flex flex-col items-center w-full px-2 space-y-2">
-            <button type="submit" className="bg-orange-500 text-white text-center font-semibold w-full rounded shadow-lg hover:scale-105 py-1 duration-75">
-              Entrar
-            </button>
-            <button className="border-b w-1/2 text-gray-500 text-xs hover:scale-105 py-1 duration-75">
-              Esqueci minha senha
-            </button>
-          </div>
+          <button type='button' className="bg-gray-300 rounded-lg w-1/3 h-1/2 text-xs shadow-lg text-white hover:scale-105 duration-75">
+            Esqueci minha senha
+          </button>
         </form>
       </div>
     </div>
