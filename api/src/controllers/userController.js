a8c83b02ce996f09c4dc51aa4f76fdb02e210865
Samuel Lemos <samueltdelemos@gmail.com>
2024-04-17 16:38:23 -0300

    feat: adicionando metodo de login

--- api/src/controllers/userController.js
+++ api/src/controllers/userController.js
@@ -1,8 +1,12 @@
+// biblioteca para encriptar a senha
 const bcrypt = require('bcrypt');
+
+// importando a model
 const userModel = require('../models/userModel');
 
 // função que valida o email
 const { validateEmail } = require('../utils/validators');
+
 
 // retorna todos os usuários
 const getAllUsers = async (req, res) => {
@@ -43,6 +47,7 @@ const getUserById = async (req, res) => {
 // cria um novo usuário
 const createUser = async (req, res) => {
   console.log('Starting createUser controller.');
+
   const userData = req.body;
 
   // verificando se todos os dados foram preenchidos antes de prosseguir
@@ -64,7 +69,6 @@ const createUser = async (req, res) => {
   try {
     // pesquisando o username fornecido para verificar se já existe
     const existingUser = await userModel.getUserByUsername(userData.username);
-    
     if (existingUser) {
       return res.status(400).json({ message: 'Username já existente.' });
     }
@@ -78,13 +82,14 @@ const createUser = async (req, res) => {
 
     const newUser = await userModel.createUser(userData);
     return res.status(201).json(newUser);
+
   } catch (error) {
     console.error("Error creating user:", error.message);
     return res.status(500).json({ message: 'Erro ao criar usuário.' });
   }  
 };
 
-// edita as informações de um usuário (o usuário pode somente editar suas próprias informações)
+// edita as informações de um usuário (o usuário pode editar somente suas próprias informações)
 const editUser = async (req, res) => {
   console.log('Starting editUser controller.');
     
@@ -99,7 +104,39 @@ const deleteUser = async (req, res) => {
 // login e autenticação
 const login = async (req, res) => {
   console.log('Starting login controller.');
-    
+
+  const userData = req.body;
+
+  // verificando se todos os dados foram preenchidos antes de prosseguir
+  if (!userData || !userData.email || !userData.password) {
+    return res.status(400).json({ message: 'Dados inválidos ou não preenchidos.' });
+  }
+
+  // chamando o validador de email
+  validateEmail(userData.email)
+
+  try {
+    // pesquisando o username fornecido para verificar se o usuário existe
+    const existingUser = await userModel.getUserByUsername(userData.username);
+    if (!existingUser) {
+      return res.status(404).json({ message: 'Usuário não encontrado.' });
+    }
+
+    // Compara o password fornecido com o password armazenado no banco de dados usando bcrypt
+    const isPasswordValid = await bcrypt.compare(userData.password, existingUser.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: 'Senha incorreta.' });
+    }
+
+    // --------------- AQUI VAI TER O CÓDIGO PARA GERAR O TOKEN --------------------- \\
+
+    // se está tudo válido então retorna um status 200 e uma mensagem de login bem sucedido
+    return res.status(200).json({ message: 'Login efetuado com sucesso.' });
+
+  } catch (error) {
+    console.error("Error logging in:", error.message);
+    return res.status(500).json({ message: 'Erro ao fazer login.' });
+  }
 };
 
 module.exports = {
