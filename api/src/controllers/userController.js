2ef890eb31623bc7f023e7ffa28a28144e80f90b
Samuel Lemos <samueltdelemos@gmail.com>
2024-04-17 11:11:43 -0300

    feat: criando controllers para cadastro e get por id do usuário

--- api/src/controllers/userController.js
+++ api/src/controllers/userController.js
@@ -1,6 +1,7 @@
 const userModel = require('../models/userModel');
 
 const getAllUsers = async (req, res) => {
+  console.log('Starting getAllUsers controller.');
   try {
     const users = await userModel.getAllUsers();
 
@@ -16,24 +17,60 @@ const getAllUsers = async (req, res) => {
 };
 
 const getUserById = async (req, res) => {
+  console.log('Starting getUserById controller.');
+  const userId = req.params.id;
 
-}
+  try {
+    const user = await userModel.getUserById(userId);
+
+    if (!user) {
+      return res.status(404).json({ message: 'Usuário não encontrado.' });
+    }
+
+    return res.status(200).json(user);
+  } catch (error) {
+    console.error("Error fetching user by ID:", error.message);
+    return res.status(500).json({ message: 'Erro ao buscar usuário.' });
+  }
+};
 
 const createUser = async (req, res) => {
+  console.log('Starting createUser controller.');
+  const userData = req.body;
+
+  if (!userData || !userData.name || !userData.email || !userData.username || !userData.password) {
+    return res.status(400).json({ message: 'Dados inválidos.' });
+  }
+
+  try {
+    const existingUser = await userModel.getUserByUsername(userData.username);
     
-}
+    if (existingUser) {
+      return res.status(400).json({ message: 'Username já existe.' });
+    }
+
+    const newUser = await userModel.createUser(userData);
+    return res.status(201).json(newUser);
+  } catch (error) {
+    console.error("Error creating user:", error.message);
+    return res.status(500).json({ message: 'Erro ao criar usuário.' });
+  }  
+};
 
 const editUser = async (req, res) => {
+  console.log('Starting editUser controller.');
     
-}
+};
 
 const deleteUser = async (req, res) => {
+  console.log('Starting deleteUser controller.');
     
-}
+};
 
 const login = async (req, res) => {
+  console.log('Starting login controller.');
     
-}
+};
 
 module.exports = {
   getAllUsers,
--- api/src/routes/routes.js
+++ api/src/routes/routes.js
@@ -3,6 +3,11 @@ const userController = require('../controllers/userController')
 
 const router = express.Router();
 
-router.get('/user', userController.getAllUsers);
+router.get('/users', userController.getAllUsers);
+router.get('/user', userController.getUserById);
+router.post('/user', userController.createUser);
+router.put('/user', userController.editUser);
+router.delete('/user', userController.deleteUser);
+router.post('/login', userController.login);
 
 module.exports = router
