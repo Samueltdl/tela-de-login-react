b3ff2deb737728a4d0e8e11cc9006d60174e8540
Samuel Lemos <samueltdelemos@gmail.com>
2024-04-17 14:19:21 -0300

    refactor: unindo os arquivos app e server

--- api/src/server.js
+++ api/src/server.js
@@ -1,7 +1,12 @@
-const app = require('./app')
 require('dotenv').config();
+const express = require('express')
+const router = require('./routes/routes')
 
 const port = process.env.PORT || 3333;
+const app = express();
+
+app.use(express.json());
+app.use(router);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}.`)
