fcb05829ecd62c66197c163882da449f4bf2a917
Samuel Lemos <samueltdelemos@gmail.com>
2024-04-18 01:47:27 -0300

    refactor: alterando o retorno da função validateEmail para booleano

--- api/src/utils/validators.js
+++ api/src/utils/validators.js
@@ -4,10 +4,8 @@ const jwt = require('jsonwebtoken');
 // Verifica o formato do email usando regex
 const validateEmail = (email) => {
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  if (!emailRegex.test(email)) {
-    return res.status(400).json({ message: 'Email inválido.' });
-  }
-}
+  return emailRegex.test(email);
+};
 
 const validateToken = (req, res, next) => {
   const authHeader = req.headers.authorization;
