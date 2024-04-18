87d697a67621c29235d7cd4ba2b9c2faa41820be
Samuel Lemos <samueltdelemos@gmail.com>
2024-04-18 01:48:29 -0300

    feat: implementando o método de edit do usuário

--- api/src/controllers/userController.js
+++ api/src/controllers/userController.js
@@ -51,8 +51,10 @@ const createUser = async (req, res) => {
     return res.status(400).json({ message: 'Dados inválidos ou não preenchidos.' });
   }
 
-  // chamando o validador de email
-  validateEmail(userData.email)
+  // Validar o formato do email
+  if (!validateEmail(userData.email)) {
+    return res.status(400).json({ message: 'Formato de email inválido.' });
+  }
 
   // verificando se as senhas coincidem
   if (userData.password !== userData.confirmPassword){
@@ -88,7 +90,49 @@ const createUser = async (req, res) => {
 // edita as informações de um usuário (o usuário pode editar somente suas próprias informações)
 const editUser = async (req, res) => {
   console.log('Starting editUser controller.');
-    
+  
+  const userData = req.body;
+
+  // verificando se todos os dados foram preenchidos antes de prosseguir
+  if (!userData || !userData.name || !userData.email || !userData.username || !userData.password || !userData.confirmPassword) {
+    return res.status(400).json({ message: 'Dados inválidos ou não preenchidos.' });
+  }
+
+  // validar o formato do email
+  if (!validateEmail(userData.email)) {
+    return res.status(400).json({ message: 'Formato de email inválido.' });
+  }
+
+  // verificando se as senhas coincidem
+  if (userData.password !== userData.confirmPassword){
+    return res.status(400).json({message: 'As senhas não coincidem.'});
+  }
+
+  // removendo o campo de confirmPassword do json antes de chamar a model
+  delete userData.confirmPassword;
+
+  try {
+    // pesquisando o username fornecido para verificar se já existe e é diferente do username antigo caso tenha sido alterado
+    const user = await userModel.getUserById(userData.id);
+    const existingUser = await userModel.getUserByUsername(userData.username);
+    if (existingUser && existingUser.username !== user.username) {
+      return res.status(400).json({ message: 'Username já existente.' });
+    }
+
+    // caso a senha tenha sido alterada, ela será encriptada antes de prosseguir
+    if (userData.password !== user.password) {
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(userData.password, salt);
+      userData.password = hashedPassword;
+    };
+
+    const newUser = await userModel.editUser(userData);
+    return res.status(201).json(newUser);
+
+  } catch (error) {
+    console.error("Error editing user:", error.message);
+    return res.status(500).json({ message: 'Erro ao editar usuário.' });
+  }
 };
 
 // deleta seu próprio usuário
--- api/src/models/userModel.js
+++ api/src/models/userModel.js
@@ -85,9 +85,34 @@ const createUser = async (userData) => {
 }
 
 // edita o usuário especficado na requisição no banco
-const editUser = async () => {
-  console.log('Starting editUser model.')
-}
+const editUser = async (user) => {
+  const { id, name, email, username, password } = user;
+
+  try {
+    const pool = await connectDatabase();
+    
+    const query = `
+      UPDATE users
+      SET name = $1, email = $2, username = $3
+      WHERE id = $4
+      RETURNING *
+    `;
+
+    const values = [name, email, username, id];
+
+    // Se a senha foi fornecida, atualize-a
+    if (password) {
+      
+      query.replace('SET', 'SET password = $5,');
+    }
+
+    const { rows } = await pool.query(query, values);
+    return rows[0];
+
+  } catch (error) {
+    throw new Error(`Error updating user: ${error.message}`);
+  }
+};
 
 // delete o usuário especificado na requisição do banco
 const deleteUser = async () => {
