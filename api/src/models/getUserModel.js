efc29f8d39aaf9b5709bf0b0a8569c95ce14b891
Samuel Lemos <samueltdelemos@gmail.com>
2024-04-18 22:30:55 -0300

    refactor: alterando o getAllUser para retornar somente name e username

--- api/src/models/getUserModel.js
+++ api/src/models/getUserModel.js
@@ -5,54 +5,54 @@ const getAllUsers = async () => {
     console.log('Starting getAllUsers model.')
     try {
       const pool = await connectDatabase();
-      const { rows } = await pool.query('SELECT * FROM users');
+      const { rows } = await pool.query('SELECT name, username FROM users');
       return rows;
     } catch (error) {
       console.error("Error fetching all users:", error.message);
       throw error;
     }
   };
-  
-  // retorna somente o usuário do banco com o id especificado na requisição
-  const getUserById = async (userId) => {
-    console.log('Starting getUserById model.')
-    try {
-      const pool = await connectDatabase();
-      const { rows } = await pool.query('SELECT * FROM users WHERE id = $1', [userId]);
-      
-      if (rows && rows.length > 0) {
-        return rows[0];
-      } else {
-        return null;
-      }
-    } catch (error) {
-      console.error("Error fetching user by ID:", error.message);
-      throw error;
+
+// retorna somente o usuário do banco com o id especificado na requisição
+const getUserById = async (userId) => {
+  console.log('Starting getUserById model.')
+  try {
+    const pool = await connectDatabase();
+    const { rows } = await pool.query('SELECT * FROM users WHERE id = $1', [userId]);
+    
+    if (rows && rows.length > 0) {
+      return rows[0];
+    } else {
+      return null;
     }
-  };
-  
-  // retorna um usuário caso já exista com o username requisitado
-  const getUserByUsername = async (username) => {
-    console.log('Starting getUserByUsername model.')
-    try {
-      const pool = await connectDatabase();
-      
-      const query = `
-        SELECT * FROM users WHERE username = $1;
-      `;
-  
-      const { rows } = await pool.query(query, [username]);
-  
-      if (rows && rows.length > 0) {
-        return rows[0];
-      } else {
-        return null;
-      }
-    } catch (error) {
-      console.error("Error fetching user by username:", error.message);
-      throw error;
+  } catch (error) {
+    console.error("Error fetching user by ID:", error.message);
+    throw error;
+  }
+};
+
+// retorna um usuário caso já exista com o username requisitado
+const getUserByUsername = async (username) => {
+  console.log('Starting getUserByUsername model.')
+  try {
+    const pool = await connectDatabase();
+    
+    const query = `
+      SELECT * FROM users WHERE username = $1;
+    `;
+
+    const { rows } = await pool.query(query, [username]);
+
+    if (rows && rows.length > 0) {
+      return rows[0];
+    } else {
+      return null;
     }
-  };
+  } catch (error) {
+    console.error("Error fetching user by username:", error.message);
+    throw error;
+  }
+};
 
 module.exports = {
     getAllUsers,
