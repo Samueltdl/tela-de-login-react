ca9f422f1da5aad5047094dd065dbc12e533e510
Samuel Lemos <samueltdelemos@gmail.com>
2024-04-22 16:42:54 -0300

    fix: corrigindo problema de importação da variável de ambiente no arquivo de config da api

--- client/src/services/config.jsx
+++ client/src/services/config.jsx
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const Api  = axios.create({
-    baseURL:"http://localhost:3000/",
-    headers:{
+    baseURL: import.meta.env.REACT_APP_API,
+    headers: {
         "Content-Type":"application/json",
-        Accept:"*/*",
+        Accept: "*/*",
         //withCredentials: true,
         Authorization:`Bearer ${localStorage.getItem("token")}`
     },
