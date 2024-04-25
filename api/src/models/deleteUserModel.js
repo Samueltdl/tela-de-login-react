a818a529133691ed6fc8b95c6ad796376d359ff3
Samuel Lemos <samueltdelemos@gmail.com>
2024-04-25 20:50:23 -0300

    feat: criando model de inativação de usuário (os usuários não serão deltados do banco, mas sim marcados como desativados)

--- api/src/models/deleteUserModel.js
+++ api/src/models/deleteUserModel.js
@@ -1,8 +1,28 @@
 const connectDatabase = require('../database/db'); // importando conexão com o banco de dados
 
-// delete o usuário especificado na requisição do banco
-const deleteUser = async () => {
+// inativa o usuário especificado
+const deleteUser = async (user) => {
     console.log('Starting deleteUser model.');
+    const { id } = user;
+
+    try {
+      const pool = await connectDatabase();
+      
+      const query = `
+        UPDATE users
+        SET isactive = $1
+        WHERE id = $2
+        RETURNING *
+      `;
+
+      const values = [false, id];
+
+      const { rows } = await pool.query(query, values);
+      return rows[0];
+  
+    } catch (error) {
+      throw new Error(`Error deactivating user: ${error.message}`);
+    }
 }
 
 module.exports = deleteUser;
