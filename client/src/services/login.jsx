fbc6d964666ecf8c0bce22e4d0794759ff9e3c23
Samuel Lemos <samueltdelemos@gmail.com>
2024-04-26 17:43:52 -0300

    fix: corrigindo problema das mensagens de erro na requisição de login

--- client/src/services/login.jsx
+++ client/src/services/login.jsx
@@ -11,23 +11,31 @@ export const useLogin = () => {
     try {
       setLoading(true);
       const response = await Api.post("/login", data);
-      //console.log(response)
-      if (response.status === 200) {
-        setLoading(false);
 
-        localStorage.setItem("userId", response.data.userId)
-        localStorage.setItem("token", response.data.token)
+      // armazenando dados essenciais no localstorage (não é a forma mais adequada e pretendo mudar isso futuramente)
+      localStorage.setItem("userId", response.data.userId)
+      localStorage.setItem("token", response.data.token)
 
-        return alert(response.data.message)
-      }
-      
+      //alert(response.data.message)
+
       setLoading(false);
-      return alert(`${response.data.message} \n\n Código do erro: ${response.status}`)
 
     } catch (error) {
-      //console.log(error);
       setLoading(false);
-      return alert("Ocorreu um erro ao fazer login\n\n" + "Código do erro: " + error.message);
+
+      if (error.response) {
+        // a resposta foi recebida, mas tem um status diferente de 2xx
+        const errorMessage = error.response.data.message || "Ocorreu um erro ao fazer login.";
+        alert(`${errorMessage}\n\nCódigo do erro: ${error.response.status}`);
+      
+      } else if (error.request) {
+        // a solicitação foi feita, mas não recebeu resposta
+        alert("Não foi recebida resposta do servidor.");
+      
+      } else {
+        // ocorreu um erro durante a configuração da solicitação
+        alert("Ocorreu um erro ao enviar a requisição.");
+      }
     }
   };
 
