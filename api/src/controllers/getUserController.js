169782512f4e6dba35e36d59440bb18f5d8cd9d0
Samuel Lemos <samueltdelemos@gmail.com>
2024-04-27 14:48:09 -0300

    feat: adicionando verificação de usuário ativo nos controllers de get

--- api/src/controllers/getUserController.js
+++ api/src/controllers/getUserController.js
@@ -3,7 +3,16 @@ const userModelInterface = require('../models/userModelInterface') // importando
 // retorna todos os usuários
 const getAllUsers = async (req, res) => {
   console.log('Starting getAllUsers controller.');
+
+  const userData = req.body
+
   try {
+    const existingUser = await userModelInterface.getUserById(userData.userId);
+    // verifica se o usuário existe e está ativo para ser possível prosseguir
+    if (!existingUser || existingUser.is_active === false) {
+      return res.status(404).json({ message: 'Usuário inválido.' })
+    }
+
     const users = await userModelInterface.getAllUsers();
     
     // se não possui nenhum usuário no banco então retorna uma mensagem informando que nenhum usuári foi encontrado
@@ -22,21 +31,29 @@ const getAllUsers = async (req, res) => {
 // retorna somente o usuário com o id especificado na requisição
 const getUserById = async (req, res) => {
   console.log('Starting getUserById controller.');
-  const userId = req.body.userId;
+
+  const userData = req.body
+  const searchId = req.body.searchId;
 
   try {
-    const user = await userModelInterface.getUserById(userId);
+    const existingUser = await userModelInterface.getUserById(userData.userId);
+    // verifica se o usuário existe e está ativo para ser possível prosseguir
+    if (!existingUser || existingUser.is_active === false) {
+      return res.status(404).json({ message: 'Usuário inválido.' })
+    }
+
+    const searchUser = await userModelInterface.getUserById(searchId);
 
     // caso não encontre o usuário correspondente ao id então retorna uma mensagem informando que o usuário não foi encontrado
-    if (!user) {
+    if (!searchUser) {
       return res.status(404).json({ message: 'Usuário não encontrado.' });
     }
 
     return res.status(200).json({
-      userId:user.user_id,
-      name:user.name,
-      username:user.username,
-      registrationDate:user.registration_date
+      userId:searchUser.user_id,
+      name:searchUser.name,
+      username:searchUser.username,
+      registrationDate:searchUser.registration_date
     });
 
   } catch (error) {
@@ -48,21 +65,29 @@ const getUserById = async (req, res) => {
 // retorna somente o usuário com o id especificado na requisição
 const getUserByUsername = async (req, res) => {
   console.log('Starting getUserByUsername controller.');
-  const userUsername = req.body.username;
+
+  const userData = req.body
+  const searchUsername = req.body.searchUsername;
 
   try {
-    const user = await userModelInterface.getUserByUsername(userUsername);
+    const existingUser = await userModelInterface.getUserById(userData.userId);
+    // verifica se o usuário existe e está ativo para ser possível prosseguir
+    if (!existingUser || existingUser.is_active === false) {
+      return res.status(404).json({ message: 'Usuário inválido.' })
+    }
+
+    const searchUser = await userModelInterface.getUserByUsername(searchUsername);
 
     // caso não encontre o usuário correspondente ao username então retorna uma mensagem informando que o usuário não foi encontrado
-    if (!user) {
+    if (!searchUser) {
       return res.status(404).json({ message: 'Usuário não encontrado.' });
     }
 
     return res.status(200).json({
-      userId:user.user_id,
-      name:user.name,
-      username:user.username,
-      registrationDate:user.registration_date
+      userId:searchUser.user_id,
+      name:searchUser.name,
+      username:searchUser.username,
+      registrationDate:searchUser.registration_date
     });
 
   } catch (error) {
