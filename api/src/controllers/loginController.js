6ad87af7b62766f55ef50ac9831d3137380cfd26
Samuel Lemos <samueltdelemos@gmail.com>
2024-04-27 13:08:00 -0300

    refactor: alterando a mensagem que é retornada caso o usário digite alguma credencial inválida no login

--- api/src/controllers/loginController.js
+++ api/src/controllers/loginController.js
@@ -21,29 +21,32 @@ const login = async (req, res) => {
     }
 
     try {
-      // pesquisando o username fornecido para verificar se o usuário existe
+      // pesquisando o email fornecido para verificar se o usuário existe
       const existingUser = await userModelInterface.getUserByEmail(userData.email);
       if (!existingUser) {
-        return res.status(404).json({ message: 'Usuário não encontrado.' });
+        // Aqui retorna email ou senha incorretos propositalmente para dificultar o acesso de pessoas que não o proprietário da conta
+        return res.status(404).json({ message: 'E-mail ou senha incorretos.' });
       }
   
       // Compara o password fornecido com o password armazenado no banco de dados usando bcrypt
       const isPasswordValid = await bcrypt.compare(userData.password, existingUser.password);
       if (!isPasswordValid) {
-        return res.status(401).json({ message: 'Senha incorreta.' });
+        // Aqui é o mesmo cado da verificação de email acima, retorna email ou senha incorretos para dificultar o acesso de terceiros
+        return res.status(401).json({ message: 'E-mail ou senha incorretos.' });
       }
-
+      
+      // verifica se o usuário está ativo para ser possível efetuar o login
       if (existingUser.is_active === false) {
         return res.status(404).json({ message: 'Usuário não encontrado.' })
       }
       
       const token = jwt.sign(
-        { userId: existingUser.user_id, email: userData.email },
-        process.env.TOKEN_SECRET_KEY,
-        { expiresIn: '1h' }
+        { userId: existingUser.user_id, email: userData.email }, // dados a serem armazenados no token
+        process.env.TOKEN_SECRET_KEY, // obtendo chave secreta da variável de ambiente
+        { expiresIn: '1h' } // tempo de expiração do token
       );
 
-      // se está tudo válido então retorna um status 200 e uma mensagem de login bem sucedido e o token
+      // se está tudo válido então retorna um status 200 e uma mensagem de login bem sucedido, o id do usuário e o token
       return res.status(200).json({ 
         message: 'Login efetuado com sucesso.',
         userId: `${existingUser.user_id}`,
