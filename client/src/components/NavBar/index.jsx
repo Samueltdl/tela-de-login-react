0eae3baab6d482c84bb4bba64bdcdbe33b47adf6
Samuel Lemos <samueltdelemos@gmail.com>
2024-05-04 16:05:28 -0300

    feat: adicionando mais alguns elementos na página inicial

---
+++ client/src/components/NavBar/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function NavBar () {
+    return (
+        <div className="fixed top-0 w-full h-12 bg-gray-400">
+            <button  className="fixed top-2 end-4 h-7 w-14 bg-blue-400 hover:bg-blue-500 text-white rounded">
+                Sair
+            </button>
+
+        </div>
+    );
+}
--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,6 +1,10 @@
-import React from 'react'
+import React from 'react';
 
-import { useGetAllUsers } from '../services/getAllUsers'
+import { useGetAllUsers } from '../services/getAllUsers'; // importando hook de requisição
+
+// components
+import Loader from '../components/loader';
+import NavBar from '../components/NavBar';
 
 export default function Home() {
   const { usersList, loading } = useGetAllUsers();
@@ -8,14 +12,18 @@ export default function Home() {
   console.log(usersList)
 
   return (
-    <div className="fixed inset-0 bg-blue-200 flex justify-center items-center lg:p-4">
-        <div className='grid grid-cols-2 space-x-4 bg-purple-100 rounded-lg w-full h-full'>
+    <>
+    {loading ? <Loader /> : null}
+      <div className="fixed inset-0 overflow-y-auto bg-blue-200 flex flex-col justify-center items-center"> 
+        <NavBar />
+      
+        <div className='grid grid-cols-2 space-x-4 rounded-lg w-full h-full p-10'>
 
-          <div className='p-4'>
+          <div className='p-4 bg-white rounded-lg shadow-md shadow-gray-900'>
             infos do usuário logado
           </div>
 
-          <div className='p-4'>
+          <div className='p-4 bg-white rounded-lg shadow-md shadow-gray-900'>
             <div className="grid grid-cols-2 h-10 mx-3 my-1.5 px-4 rounded bg-white hover:bg-gray-100 text-sm">
               <p className="flex items-center">Nome:</p>
               <p className="flex items-center">Nome de usuário:</p>
@@ -30,6 +38,8 @@ export default function Home() {
           </div>
           
         </div>
-    </div>
+
+      </div>
+    </>
   )
 }
