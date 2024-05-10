3b9ff6b992164bbbd71d5647037641b46f99e723
Samuel Lemos <samueltdelemos@gmail.com>
2024-05-09 23:43:22 -0300

    feat: criando requisição de get das informações do usuário que está logado

--- client/src/services/getUser.jsx
+++ client/src/services/getUser.jsx
@@ -46,3 +46,47 @@ export const useGetAllUsers = () => {
 
   return { usersList, loading };
 };
+
+// requisção para dar get nas informações do usuário que está logado
+export const useGetUserLoged = () => {
+
+  const [user, setUser] = useState();
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+  const getUserLoged = async () => {
+
+    try {
+      setLoading(true);
+      
+      const response = await Api.get("/userLoged");
+
+      setUser(response.data);
+
+      setLoading(false);
+    } 
+
+    catch (error) {
+      setLoading(false);
+
+      if (error.response) {
+        // a resposta foi recebida, mas tem um status diferente de 2xx
+        const errorMessage = error.response.data.message || "Ocorreu um erro ao buscar as informações do usuário.";
+        alert(`${errorMessage}\n\nCódigo do erro: ${error.response.status}`);
+      
+      } else if (error.request) {
+        // a solicitação foi feita, mas não recebeu resposta
+        alert("Não foi recebida resposta do servidor.");
+      
+      } else {
+        // ocorreu um erro durante a configuração da solicitação
+        alert("Ocorreu um erro ao enviar a requisição.");
+      }
+    }
+  };
+
+  getUserLoged();
+  }, []);
+
+  return { user, loading };
+};
