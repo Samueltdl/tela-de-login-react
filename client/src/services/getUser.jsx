cf6407439c157ebcb7d22e44162e15a093794ebe
Samuel Lemos <samueltdelemos@gmail.com>
2024-05-15 23:47:31 -0300

    feat: criando requisição de get de usuário pelo username

--- client/src/services/getUser.jsx
+++ client/src/services/getUser.jsx
@@ -90,3 +90,47 @@ export const useGetUserLoged = () => {
 
   return { user, loading };
 };
+
+// requisição para dar get em todos os usuários (a api retorna apenas o name e o username)
+export const useGetUserByUsername = (username) => {
+
+  const [usersByUsernameList, setUsersByUsernameList] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+  const getUserByUsername = async () => {
+
+    try {
+      setLoading(true);
+      
+      const response = await Api.get(`/user?username=${username}`);
+
+      setUsersByUsernameList(response.data);
+
+      setLoading(false);
+    } 
+
+    catch (error) {
+      setLoading(false);
+
+      if (error.response) {
+        // a resposta foi recebida, mas tem um status diferente de 2xx
+        const errorMessage = error.response.data.message || "Ocorreu um erro ao buscar a lista de usuários.";
+        alert(`${errorMessage}\n\nCódigo do erro: ${error.response.status}`);
+      
+      } else if (error.request) {
+        // a solicitação foi feita, mas não recebeu resposta
+        alert("Não foi recebida resposta do servidor.");
+      
+      } else {
+        // ocorreu um erro durante a configuração da solicitação
+        alert("Ocorreu um erro ao enviar a requisição.");
+      }
+    }
+  };
+
+  getUserByUsername();
+  }, []);
+
+  return { usersByUsernameList, loading };
+};
